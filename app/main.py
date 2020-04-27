4b0e609ad7879d15a1e97ec20acf0a243bfa9b4e
yuki <o.yukinko0715@gmail.com>
2020-04-27 22:51:04 +0900

    add setting config

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
+
+config_type = {
+    "development":  "config.Development",
+    "testing": "config.Testing",
+}
+
+app.config.from_object(config_type.get(os.getenv('FLASK_APP_ENV', 'development')))
 
 
 @app.route('/')
