db7c8669b4085c007780214e6b6ff59e2fa0a938
yuki <o.yukinko0715@gmail.com>
2020-04-27 22:47:41 +0900

    add db setting

---
+++ app/database/setting.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_utils import database_exists, create_database
+from flask import current_app
+from config import Base
+
+
+def initialize_db():
+    engine = create_engine(Base.DATABASE)
+    if not database_exists(engine.url):
+        create_database(engine.url)
+
+
+ENGINE = create_engine(
+    Base.DATABASE,
+    encoding="utf-8",
+    echo=True
+)
+
+session = scoped_session(
+    # ORM実行時の設定。自動コミットするか、自動反映するなど。
+    sessionmaker(
+        autocommit=False,
+        autoflush=False,
+        bind=ENGINE
+    )
+)
+
+DB_Base = declarative_base()
