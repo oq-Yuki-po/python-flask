1901f43a5e37b1fc78ea3d23ec8ddc385b88d49e
yuki <o.yukinko0715@gmail.com>
2020-05-01 23:05:17 +0900

    Add error messages

---
+++ app/resources/message/error.py
@@ -0,0 +1,6 @@
+
+MESSAGE = {
+    'InternalServerError': 'unexpected error has occurred on the server.',
+    'RequestJsonValidationError': 'format of the request is incorrect.',
+    'DataBaseConnecitonError': 'error occurred in the DB connection.'
+}
