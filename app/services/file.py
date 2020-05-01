f1a211340c3518dc961cdabe57578404db033325
yuki <o.yukinko0715@gmail.com>
2020-05-02 05:04:47 +0900

    Changed the way SQLAlchemy is executed

--- app/services/file.py
+++ app/services/file.py
@@ -1,9 +1,10 @@
 import typing as tp
+from datetime import datetime
 
 from sqlalchemy.exc import SQLAlchemyError, DataError
 from api.requests.classes import File as ApiFile
 from api.responses.errors import DataBaseConnecitonError, DataBaseDataError
-from database.factories import UserFactory, FileFactory, FileDetailFactory
+from database.models import FileDetail, File, User
 from database.models.setting import Session
 
 
@@ -18,14 +19,20 @@ class FileService():
         try:
             session = Session()
 
-            user = UserFactory.build(name=self.file.user_name)
-            file = FileFactory.build(users=user, name=self.file.name)
+            created_at = datetime.now()
+
+            user = User(name=self.file.user_name,
+                        created_at=created_at, updated_at=created_at)
+
+            file = File(users=user, name=self.file.name,
+                        created_at=created_at, updated_at=created_at)
+
             session.add(user)
             session.add(file)
-            for detail in self.file.details:
-                session.add(FileDetailFactory.build(files=file,
-                                                    row=detail.row,
-                                                    contents=detail.contents))
+
+            details = [{'row': d.row, 'contents': d.contents, 'updated_at': created_at,
+                        'created_at': created_at} for d in self.file.details]
+            session.execute(FileDetail.__table__.insert(), details)
 
             session.commit()
 
