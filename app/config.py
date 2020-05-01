8de356eaf9d5baf77a97c58e0a1fc84e138783fc
yuki <o.yukinko0715@gmail.com>
2020-05-01 22:56:48 +0900

    Add config values

--- app/config.py
+++ app/config.py
@@ -1,19 +1,27 @@
 # config.py
 import os
+import logging
 
 
 class Base(object):
+    API_VERSION = '/v1'
     DB_USER = os.environ.get('POSTGRES_USER')
     DB_PASS = os.environ.get('POSTGRES_PASSWORD')
     DB_URL = os.environ.get('POSTGRES_URL')
     DB_NAME = os.environ.get('POSTGRES_DB')
     DB_PORT = os.environ.get('POSTGRES_PORT', "5432")
     DATABASE = f"postgresql://{DB_USER}:{DB_PASS}@{DB_URL}:{DB_PORT}/{DB_NAME}"
+    JSON_AS_ASCII = False
+    RESTFUL_JSON = {
+        'ensure_ascii': False
+    }
 
 
 class Development(Base):
     SQL_ECHO = False
+    LOG_LEVEL = logging.INFO
 
 
 class Testing(Base):
     SQL_ECHO = True
+    LOG_LEVEL = logging.DEBUG
